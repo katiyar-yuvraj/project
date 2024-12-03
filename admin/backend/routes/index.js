aaaecb7662966ebb1e9f6ac4a87ddc84835c233c
Roushan <kumar7roushanjha2005@gmail.com>
2024-12-04 02:11:23 +0530

    prev commit

--- admin/backend/routes/index.js
+++ admin/backend/routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require("./user.routes.js");
 const studentRouter = require("./student.routes.js");
 const attendanceRoutes = require("./attendance.routes.js"); 
+const announcementRoutes = require("./announcement.routes.js");
 
 module.exports = {
     userRoutes,
     studentRouter,
-    attendanceRoutes
+    attendanceRoutes,
+    announcementRoutes
 };
